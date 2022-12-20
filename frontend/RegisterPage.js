8d68d8c54e5234e4bd3f8c2a90caf32d23507388
63070061 <63070061@kmitl.ac.th>
2022-12-20 11:35:28 +0700

    login reg

---
+++ frontend/RegisterPage.js
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Kanit_400Regular } from '@expo-google-fonts/kanit';
+import { Input, Button } from '@ui-kitten/components';
+
+
+const Register = () => {
+  const [name, setName] = useState('');
+  const [aboutMe, setAboutMe] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmpassword, setConfirmPassword] = useState('');
+  const [alert, setAlert] = useState('');
+  let [fontsLoaded] = useFonts({
+    Kanit_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  const signup = async () => {
+
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontFamily: 'Kanit_400Regular', fontSize: 40, color: '#FDC319' }}>REGISTER</Text>
+      <View style={{ marginTop: 25 }}>
+        <Text style={styles.fontEngInputHeader}>Name</Text>
+        <Input style={styles.fontEngInput} onChangeText={text => setName(text)} />
+      </View>
+      <View style={styles.row}>
+        <Text style={styles.fontEngInputHeader}>About Me</Text>
+        <Input style={[styles.fontEngInput, styles.fontTh, styles.textarea]} onChangeText={text => setAboutMe(text)} multiline='true' numberOfLines="4" />
+      </View>
+      <View style={{ marginTop: 20 }}>
+        <Text style={styles.fontEngInputHeader}>Username</Text>
+        <Input style={styles.fontEngInput} onChangeText={text => setUsername(text)} />
+      </View>
+      <View style={{ marginTop: 20 }}>
+        <Text style={styles.fontEngInputHeader}>Password</Text>
+        <Input secureTextEntry={true} style={styles.fontEngInput} onChangeText={text => setPassword(text)} />
+      </View>
+      <View style={{ marginTop: 20 }}>
+        <Text style={styles.fontEngInputHeader}>Confirm Password</Text>
+        <Input secureTextEntry={true} style={styles.fontEngInput} onChangeText={text => setConfirmPassword(text)} />
+      </View>
+      <Text style={[styles.fontTh, { color: '#F73C3C', marginTop: 10 }]}>{alert}</Text>
+      <Button style={[styles.fontEng, styles.buttonStyle, { margin: 10 }]} onPress={signup}>{evaProps => <Text {...evaProps} style={{ color: "#4542C1", fontFamily: 'Kanit_400Regular', }}>Sign Up</Text>}</Button>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    fontFamily: 'Kanit_400Regular',
+    backgroundColor: '#4542C1',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 50,
+    width: "100%"
+  },
+  fontEng: {
+    fontFamily: 'Kanit_400Regular',
+    fontSize: 14,
+    color: '#4542C1',
+  },
+  fontEngInputHeader: {
+    fontFamily: 'Kanit_400Regular',
+    fontbSize: 14,
+    color: '#ffffff',
+  },
+  fontEngInput: {
+    fontFamily: 'Kanit_400Regular',
+    fontSize: 14,
+    color: 'black',
+    // backgroundColor: '#FFFFFF',
+    borderRadius: '30px',
+    width: 280
+  },
+  fontTh: {
+    fontFamily: 'Kanit_400Regular',
+    fontSize: 14,
+  },
+  buttonStyle: {
+    backgroundColor: '#FDC319',
+    borderColor: 'transparent',
+    borderWidth: 0,
+    borderRadius: "9000px",
+    width: 240
+  },
+  logo: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    width: 30,
+    height: 30,
+    alignSelf: 'flex-start',
+    // top: '-50px'
+  }
+});
+
+export default Register
--- frontend/loginPage.js
+++ frontend/loginPage.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Input, Button, Text } from '@ui-kitten/components';
 import { useFonts, Kanit_400Regular } from '@expo-google-fonts/kanit';
+import Register from './RegisterPage';
 
 const LoginPage = ({ navigation }) => {
     const [username, setUsername] = useState('');
@@ -23,7 +24,7 @@ const LoginPage = ({ navigation }) => {
 
     return (
         <View style={styles.container}>
-            <Text style={{ fontFamily: 'Kanit_400Regular', fontSize: 70, color: '#FDC319' }}>TungTY</Text>
+            <Text style={{ fontFamily: 'Kanit_400Regular', fontSize: 70, color: '#E84545' }}>Petro</Text>
             <View style={{ marginTop: 25 }}>
                 <Text style={styles.fontEngInputHeader} >Username</Text>
                 <Input style={[styles.fontEng, styles.fontEngInput]} value={username} onChangeText={text => setUsername(text)} />
@@ -33,11 +34,11 @@ const LoginPage = ({ navigation }) => {
                 <Input secureTextEntry={true} style={styles.fontEngInput} value={password} onChangeText={text => setPassword(text)} />
             </View>
 
-            <Text style={[styles.fontTh, { color: '#F73C3C' }]}>{alert}</Text>
-            <Button style={[styles.fontEng, styles.buttonStyle, { margin: 10 }]} onPress={login}>{evaProps => <Text {...evaProps} style={{ color: "#4542C1", fontFamily: 'Kanit_400Regular', }}>Sign In</Text>}</Button>
+            <Text style={[styles.fontTh, { color: '#903749' }]}>{alert}</Text>
+            <Button style={[styles.fontEng, styles.buttonStyle, { margin: 10 }]} onPress={login}>{evaProps => <Text {...evaProps} style={{ color: "#ffffff", fontFamily: 'Kanit_400Regular', }}>Sign In</Text>}</Button>
             <View style={{ marginTop: 10 }}>
-                <Text onPress={() => { navigation.navigate("FindParty"); }} style={{ color: "#ffffff" }} >ยังไม่มีสมาชิก</Text>
-                <Text onPress={() => { navigation.navigate("Register"); }} style={{ color: "#ffffff" }}>สมัครสมาชิกใหม่?</Text>
+                <Text style={{ color: "#ffffff" }} >ยังไม่มีสมาชิก</Text>
+                <Text onPress={() => { navigation.navigate("Register")}} style={{ color: "#ffffff" }}>สมัครสมาชิกใหม่?</Text>
                 <View styles={{ backgroundColor: '#FFFFFF' }}>
                 </View>
             </View>
@@ -50,7 +51,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         fontFamily: 'Kanit_400Regular',
-        backgroundColor: '#4542C1',
+        backgroundColor: '#2B2E4A',
         alignItems: 'center',
         justifyContent: 'center',
         padding: 50,
@@ -64,7 +65,7 @@ const styles = StyleSheet.create({
     fontEngInputHeader: {
         fontFamily: 'Kanit_400Regular',
         fontSize: 14,
-        color: '#ffffff',
+        color: '#E84545',
     },
     fontEngInput: {
         fontFamily: 'Kanit_400Regular',
@@ -80,7 +81,7 @@ const styles = StyleSheet.create({
         color: "#ffffff"
     },
     buttonStyle: {
-        backgroundColor: '#FDC319',
+        backgroundColor: '#E84545',
         borderColor: 'transparent',
         borderWidth: 0,
         borderRadius: "9000px",
